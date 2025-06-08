3e69315a5c56725d92ca2859e87043e78ed929d6
enduspirit <enduspirit@gmail.com>
2025-06-08 09:47:03 +0200

    CI/CD

--- Appointment/web_django/settings.py
+++ Appointment/web_django/settings.py
@@ -1,18 +1,32 @@
 from pathlib import Path
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'e*^2v5wk$27m-0&^7khsy3^j&ppyf4b(4s99$13tzr*p8i3!s7'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['appointment-api-l3vj.onrender.com',
     '127.0.0.1', 
     'localhost']
 
 AUTH_USER_MODEL = 'accounts.User'
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+
 
 # Application definition
 
@@ -67,16 +81,6 @@ REST_FRAMEWORK = {
 }
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -113,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Renderen szükséges
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
